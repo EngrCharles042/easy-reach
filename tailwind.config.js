b7c1c8777ba38c80f82249ae3077d1de0b2982cd
Degame042 <obicharles94@gmail.com>
2024-11-13 10:30:10 +0100

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [  
+    "./index.html",  
+    "./src/**/*.{js,ts,jsx,tsx}", // Modify if you're not using TypeScript  
+  ],  
+  theme: {  
+    extend: {},  
+  },  
+  plugins: [],  
+}
+
